a64c89f3c2bf51cbc3992710ee957a284856931c
OscarGGM <oscargilbertogarciamejia@gmail.com>
2024-07-23 20:00:49 -0600

    DB Port updated

--- config/db.js
+++ config/db.js
@@ -5,13 +5,13 @@ dotenv.config({path: '.env'});
 
 const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
     host: process.env.BD_HOST,
-    port: 3307,
+    port: process.env.DB_PORT,
     dialect: 'mysql', 
     define: {
         timestamp: true
     },
     pool: {
-        max: 5, // Máximo número de conexiones en el pool
+        max: 10, // Máximo número de conexiones en el pool
         min: 0, // Mínimo número de conexiones en el pool
         acquire: 30000, // Tiempo(ms) que tardará en establecer una conexión antes de marcar un error
         idle: 10000 // Tiempo(ms) de vida de una conexión sin ser utilizada
